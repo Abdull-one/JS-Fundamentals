31300bcb46ac9a4b96b0483b0df387f68f28ad40
Abdullahi Ali Adam <abdullahijangade@gmail.com>
2025-05-29 21:23:11 +0100

    Completed task 10: Factorial

---
+++ 10-factorial.js
@@ -0,0 +1,12 @@
+function factorial(n) {
+  n = parseInt(n);
+  if (isNaN(n) || n < 0) {
+    return 1;
+  } else if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+console.log(factorial(process.argv[2]));
